be8841713c49003c1c53cabd474da7a2a6794e1b
Aadarsh Dangi <aadarsht0001@gmail.com>
2025-08-04 03:18:08 +0530

    cookies done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import userModel from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   const { username, password } = req.body;
@@ -7,11 +8,16 @@ export const register = async (req, res) => {
       username,
       password,
     });
+   
+    const token =  jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    }); 
 
     res.status(201).json({
       message: "user is register succesfully",
       user,
     });
+    res.cookie("token", token);
   } catch (error) {
     console.error("Register error:", error);
     res.status(500).json({ message: "Internal server error" });
@@ -21,22 +27,48 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
   const { username, password } = req.body;
   try {
+
     const user = await userModel.findOne({
       username:username
     });
+
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
+
     const isPasswordValid = user.password === password; 
     if (!isPasswordValid) {
       return res.status(401).json({ message: "Invalid password" });
     }
+
     res.status(200).json({
       message: "user is login successfully",
       user,
     });
+
+
   } catch (error) {
     console.error("Login error:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const getUser = async (req,res)=>{
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }       
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await userModel.findById(decoded.id).select("-password");
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      res.status(200).json(user);
+    } catch (error) {
+      console.error("Get User error:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+   
+    
+}
